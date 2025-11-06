2d16545ad40277fed0dd5510cc1e4ee8544a2608
mariamawit2028-ux <mariamawit2028@hmacademy.org>
2025-11-06 14:05:00 +0300

    Create Time Determiner.py

---
+++ Time Determiner.py
@@ -0,0 +1,20 @@
+Hour=int(input(" Enter integer between 0 and 25"))
+while  Time < 0 or Time > 24:
+    print( " One of Range! Try again")
+if 5 <= Time <= 11:
+    print( " It's morning")
+if 12 <= Time <= 16:
+    print("It's afternoon")
+if 17 <= Time <= 20:
+    print( "It's evening")
+else:
+	print("It's night")
+
+
+
+
+
+
+
+
+
