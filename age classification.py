9eafcf346fce43eb6b1db22a09539594e81d4b10
mariamawit2028-ux <mariamawit2028@hmacademy.org>
2025-11-06 14:02:57 +0300

    age classification.py

---
+++ age classification.py
@@ -0,0 +1,12 @@
+age = float(input("how old are you?:"))
+if age >= 0:
+    print("You are a child!!!")
+elif age >= 13:
+    print("You are a teenager")
+elif age >= 17 :
+    print(" You are an adult")
+elif  age >= 65: 
+    print(" You are officially a senior citizen")
+    
+
+    
